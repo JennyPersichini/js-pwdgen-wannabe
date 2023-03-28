ed06ef2141fc87850a67e8aa17df3fb16ac66b62
JennyPersichini <nenny1096@gmail.com>
2023-03-28 15:42:20 +0200

    first step of bonus

--- main.js
+++ main.js
@@ -7,8 +7,17 @@ const surname = prompt("Qual è il tuo cognome?");
 //Chiedo il colore preferito
 const color = prompt("Qual è il tuo colore preferito?");
 
-const createPsw = `${nome}${surname}${color}21`;
+//Bonus: Al posto del 21 finale, chiedete all'utente 2 numeri e concatenate al posto del 21 finale, il primo numero diviso il secondo.
+let num1 = prompt("Quanti anni hai?");
+let num2 = prompt("In che giorno sei nato/a?");
+
+let division = num1 / num2 ;
+
+const createPsw = `${nome}${surname}${color}${division}`;
 console.log(createPsw);
 
 //Stampo nella pagina la password creata con i dati appena raccolti 
 document.getElementById("password").innerHTML = createPsw;
+
+
+
